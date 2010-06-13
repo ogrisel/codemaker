30aa44fd698ef7e36bed05df7a16a566c531a0dd
ogrisel <olivier.grisel@ensta.org>
2010-06-14 00:45:29 +0200

    HLLE is not a proper baseline for multi manifold reovery

--- examples/recover_multirolls.py
+++ examples/recover_multirolls.py
@@ -7,18 +7,11 @@ from codemaker.datasets import multirolls
 from codemaker.embedding import SDAEmbedder
 from codemaker.evaluation import Neighbors, local_match
 
-try:
-    import mdp
-except ImportError:
-    mdp = None
-
-mdp = None
-
 pl.clf()
 np.random.seed(0)
 
 n_features = 10
-n_samples = 3000
+n_samples = 5000
 n_manifolds = 5
 
 print "Generating %d embedded swissrolls with n_features=%d, n_samples=%d" % (
@@ -34,7 +27,11 @@ data, manifolds, colors = multirolls.load(
 stacked_manifolds = np.vstack([m + [10 * i, 0]
                                for i, m in enumerate(manifolds)])
 score = local_match(data, stacked_manifolds, query_size=50, ratio=1, seed=0)
-print "Average kNN score match manifolds/data:", score
+print "kNN score match manifolds/data (ground truth):", score
+
+# compute the score of a projection
+score = local_match(data, data[:,(0,1)], query_size=50, ratio=1, seed=0)
+print "kNN score match projection/data (baseline):", score
 
 # reshuffle the data since stochastic gradient descent assumes I.I.D. samples
 perm = np.random.permutation(data.shape[0])
@@ -42,8 +39,12 @@ data, colors = data[perm], colors[perm]
 
 # build model to extract the manifolds and learn a mapping / encoder to be able
 # to reproduce this on test data
-embedder = SDAEmbedder((n_features, 50, 20, 2), noise=0.1,
-                       sparsity_penalty=0.1, learning_rate=0.001, seed=0)
+embedder = SDAEmbedder((n_features, 6, 2), noise=0.0,
+                       sparsity_penalty=0.0, learning_rate=0.01, seed=0)
+
+random_code = embedder.encode(data)
+score = local_match(data, random_code, query_size=50, ratio=1, seed=0)
+print "kNN score match random code/data:", score
 
 print "Training encoder to unroll the embedded data..."
 start = time.time()
@@ -51,24 +52,8 @@ embedder.pre_train(data, epochs=500, batch_size=10)
 print "done in %ds" % (time.time() - start)
 code = embedder.encode(data)
 
-# evaluation of the quality of the embedding by comparing kNN queries from the
-# original (high dim) data and the low dim code on the one hand, and from the
-# ground truth low dim manifold and the low dim code on the other hand
-
-score_code_data = local_match(data, code, query_size=50, ratio=1, seed=0)
-print "kNN score match code/data:", score_code_data
-
-if mdp is not None:
-    # unroll the same data with HLLE
-    print "Comptuting projection using Hessian LLE model..."
-    start = time.time()
-    hlle_code = mdp.nodes.HLLENode(15, output_dim=2)(data)
-    print "done in %ds" % (time.time() - start)
-
-    score_hlle_code_data = local_match(
-        data, hlle_code, query_size=50, ratio=1, seed=0)
-    print "kNN score match hlle_code/data:", score_hlle_code_data
-
+score = local_match(data, code, query_size=50, ratio=1, seed=0)
+print "kNN score match code/data:", score
 
 # plot some 2D projections
 sp = pl.subplot(221)
@@ -79,16 +64,15 @@ sp = pl.subplot(222)
 sp.scatter(data[:, 3], data[:, 7], c=colors)
 sp.set_title("Projection to axis (x_3, x_7)")
 
-# plot the learned 2D embedding recovered by codemaker
+# plot the learned 2D embedding recovered by untrained SDAE model
 sp = pl.subplot(223)
+sp.scatter(random_code[:, 0], random_code[:, 1], c=colors)
+sp.set_title("2D manifold recovered by random projections")
+
+# plot the learned 2D embedding recovered by the SDAE model
+sp = pl.subplot(224)
 sp.scatter(code[:, 0], code[:, 1], c=colors)
 sp.set_title("2D manifold recovery by codemaker")
-
-if mdp is not None:
-    # plot the 2D embedding computed by HLLE
-    sp = pl.subplot(224)
-    sp.scatter(hlle_code[:, 0], hlle_code[:, 1], c=colors)
-    sp.set_title("2D manifold recovery by HLLE")
 
 pl.show()
 
