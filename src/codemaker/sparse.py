bd22b6dc040bf2d1379497537e00ddefa7eae017
Olivier Grisel <olivier.grisel@ensta.org>
2010-06-28 20:05:07 +0200

    paralle version of the sparse coder

--- src/codemaker/sparse.py
+++ src/codemaker/sparse.py
@@ -1,6 +1,7 @@
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 import numpy as np
 from scikits.learn.glm import LeastAngleRegression
-
 
 def sparse_encode(D, data, callback=None, max_features=10):
     """Given dictionary D, find sparse encoding of vectors in data
@@ -11,16 +12,17 @@ def sparse_encode(D, data, callback=None, max_features=10):
     D = np.asanyarray(D, dtype=np.double)
     data = np.asanyarray(data, dtype=np.double)
     data = np.atleast_2d(data)
+
+    # TODO: use a smart sparse representation instead
     encoded = np.zeros((data.shape[0], D.shape[1]), dtype=np.double)
 
     for i, code in enumerate(data):
-        # TODO: parallelize me!
         clf = LeastAngleRegression().fit(
             D, code, normalize=False, intercept=False,
             max_features=max_features)
 
         # threshold near zero values due to an implementation detail in the
-        # current LAR implementation
+        # current LARS implementation
         encoded[i][:] = np.where(abs(clf.coef_) < 1e-10,
                                np.zeros(clf.coef_.shape),
                                clf.coef_)
@@ -28,5 +30,54 @@ def sparse_encode(D, data, callback=None, max_features=10):
         if callback is not None:
             callback(i)
     return encoded
+
+
+# ugly wrapper for multiprocessing
+def _job_fun(args):
+    D, x, mf = args
+    return sparse_encode(D, x, max_features=mf)
+
+
+class SparseEncoder(object):
+    """Compute sparse codes representing data w.r.t. to a fixed dictionary
+
+    Parameters
+    ----------
+
+    dictionary : a 2D numpy array of dim (n_basis, n_features) where n_features
+                 is the number of features of the input space. Dictionary can be
+                 randomly generated, randomly sample from the dataset, extracted
+                 by a PCA of the dataset or learned using dictionary learning
+                 algorithms (not implemented yet).
+
+    max_features : maximum number of non zero components in the sparse code
+
+    n_cores : number of cores to use to parallelize the sparse coding
+              processing. Use None for autodetection (default), 1 to disable
+              parallelism.
+
+    """
+
+    def __init__(self, dictionary, max_features=10, n_cores=None):
+        self.dictionary = np.atleast_2d(dictionary)
+        self.max_features = max_features
+        self.n_cores = cpu_count() if n_cores is None else n_cores
+        if self.n_cores > 1:
+            self._pool = Pool(processes=n_cores)
+
+    def __call__(self, data, max_features=None):
+        """Encode data against the reference dictionary"""
+        if max_features is None:
+            max_features = self.max_features
+
+        if self.n_cores == 1:
+            # single process call of the sparse_encode function
+            return sparse_encode(self.dictionary, data, max_features)
+        else:
+            # split data in almost equals parts for parallelization
+            job_args = [(self.dictionary, chunk, max_features)
+                        for chunk in np.array_split(data, self.n_cores * 2)]
+
+            return np.vstack(self._pool.map(_job_fun, job_args))
 
 
--- tests/test_sparsecode.py
+++ tests/test_sparsecode.py
@@ -1,4 +1,5 @@
 from codemaker.sparse import sparse_encode
+from codemaker.sparse import SparseEncoder
 import numpy as np
 from nose.tools import *
 from numpy.testing import assert_almost_equal
@@ -31,5 +32,25 @@ def test_random_sparsecode():
     # check that the remaining samples could also be encoding with a sparse code
     avg_nb_nonzeros = (encoded[n_basis:] != 0).sum(axis=1).mean()
     assert avg_nb_nonzeros <= 10
+
+
+def test_parallel_sparse_code():
+    n_samples = 50
+    n_features = 20
+    n_basis = 10
+
+    np.random.seed(0)
+
+    # sample data from centered unit variance guaussian process
+    x = np.random.normal(size=(n_samples, n_features))
+
+    # lets take the first samples as dictionary
+    D = x[:n_basis].T
+
+    encoded_1 = sparse_encode(D, x, max_features=3)
+    encoded_2 = SparseEncoder(D, max_features=3, n_cores=2)(x)
+
+    assert_almost_equal(encoded_1, encoded_2)
+
 
 
