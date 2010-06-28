dd2a95c6cfc8126bae6cc246b4cac428789cb0b3
Olivier Grisel <olivier.grisel@ensta.org>
2010-06-28 18:35:03 +0200

    switch sparse coder implementation to Least Angle Regression

--- src/codemaker/sparse.py
+++ src/codemaker/sparse.py
@@ -1,14 +1,12 @@
 import numpy as np
-from scikits.learn.glm import ElasticNetCV
-from scikits.learn.glm import LassoCV
-#from scikits.learn.glm import LeastAngleRegression
-# TODO: use LARS instead of coordinate descent variants of LASSO
+from scikits.learn.glm import LeastAngleRegression
 
-def sparse_encode(D, data, callback=None, rho=1.0, n_alphas=5, eps=1e-2):
+
+def sparse_encode(D, data, callback=None, max_features=10):
     """Given dictionary D, find sparse encoding of vectors in data
 
-    Encoding is performed using coordinate descent of the elastic net
-    regularized least squares.
+    Encoding is performed using Least Angle Regression with a predifined maximum
+    number of non-zero components set to 10 by default.
     """
     D = np.asanyarray(D, dtype=np.double)
     data = np.asanyarray(data, dtype=np.double)
@@ -16,13 +14,17 @@ def sparse_encode(D, data, callback=None, rho=1.0, n_alphas=5, eps=1e-2):
     encoded = np.zeros((data.shape[0], D.shape[1]), dtype=np.double)
 
     for i, code in enumerate(data):
-        # TODO: parallelize me with multiprocessing!
-        if rho == 1.0:
-            clf = LassoCV(n_alphas=n_alphas, eps=eps).fit(D, code)
-        else:
-            clf = ElasticNetCV(
-                n_alphas=n_alphas, eps=eps, rho=rho).fit(D, code)
-        encoded[i][:] = clf.coef_
+        # TODO: parallelize me!
+        clf = LeastAngleRegression().fit(
+            D, code, normalize=False, intercept=False,
+            max_features=max_features)
+
+        # threshold near zero values due to an implementation detail in the
+        # current LAR implementation
+        encoded[i][:] = np.where(abs(clf.coef_) < 1e-10,
+                               np.zeros(clf.coef_.shape),
+                               clf.coef_)
+
         if callback is not None:
             callback(i)
     return encoded
--- tests/test_sparsecode.py
+++ tests/test_sparsecode.py
@@ -1,19 +1,18 @@
 from codemaker.sparse import sparse_encode
 import numpy as np
 from nose.tools import *
+from numpy.testing import assert_almost_equal
 
 def test_random_sparsecode():
 
-    n_samples = 100
+    n_samples = 500
     n_features = 50
     n_basis = 30
 
     np.random.seed(0)
 
     # sample data from centered unit variance guaussian process
-    x = np.random.normal(
-        loc=0.0, scale=1.0, size=n_samples * n_features).reshape(
-            n_samples, n_features)
+    x = np.random.normal(loc=0.0, scale=1.0, size=(n_samples, n_features))
 
     # lets take the first samples as dictionary
     D = x[:n_basis].T
@@ -24,18 +23,13 @@ def test_random_sparsecode():
 
     # check that the first sample are encoded using a single component (trivial
     # sparse code)
-    avg_density = (encoded[:n_basis] != 0).sum(axis=1).mean()
-    assert_almost_equal(avg_density, 1.0, 2)
-
     for i in xrange(n_basis):
-        for j in xrange(n_basis):
-            if i == j:
-                assert_almost_equal(encoded[i][j], 1.0, 1)
-            else:
-                assert_almost_equal(encoded[i][j], 0.0, 1)
+        expected_code = np.zeros(n_basis)
+        expected_code[i] = 1.0
+        assert_almost_equal(encoded[i], expected_code, decimal=2)
 
     # check that the remaining samples could also be encoding with a sparse code
     avg_nb_nonzeros = (encoded[n_basis:] != 0).sum(axis=1).mean()
-    assert_almost_equal(avg_nb_nonzeros, 2, 0)
+    assert avg_nb_nonzeros <= 10
 
 
